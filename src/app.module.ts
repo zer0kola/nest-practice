3afc5c769916208c56f6db7d2a7c7b6990fe5373
zer0kola <zer0kola321@gmail.com>
2025-03-17 00:07:39 +0900

    feat: TypeORM 설정 및 Article 엔티티 정의 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ArticleModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'test',
+      password: process.env.DB_PASSWORD || 'test',
+      database: process.env.DB_DATABASE || 'inflearn',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/article/article.entity.ts
+++ src/article/article.entity.ts
@@ -1,6 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Article {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+  })
   id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
   title: string;
+
+  @Column({ type: 'text', nullable: false })
   content: string;
+
+  @Column({
+    type: 'int',
+    name: 'author_id',
+    nullable: false,
+  })
   authorId: number;
 }
